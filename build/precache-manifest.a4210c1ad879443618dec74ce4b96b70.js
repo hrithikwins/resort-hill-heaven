2b9d5a2e12b3791efbb445560eb49f994f5a243d
HRITHIK <hrithikrtiwari@gmail.com>
2020-07-28 21:31:37 +0530

    some new changes

---
+++ build/precache-manifest.a4210c1ad879443618dec74ce4b96b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9058a511fbf138d9b94c3250fc1a5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e78a4125b92eb16e8f96",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b93252e4e6ffdb61c047",
+    "url": "/static/css/main.5493f32a.chunk.css"
+  },
+  {
+    "revision": "e78a4125b92eb16e8f96",
+    "url": "/static/js/2.a6e5d0e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6e5d0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93252e4e6ffdb61c047",
+    "url": "/static/js/main.9505a723.chunk.js"
+  },
+  {
+    "revision": "41a31e5fe3bc7be8a548",
+    "url": "/static/js/runtime-main.039619c0.js"
+  }
+]);
