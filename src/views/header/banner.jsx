d1d18160f32d65d948bc1dedb9ef0cc3136824de
HRITHIK <hrithikrtiwari@gmail.com>
2020-08-05 12:03:01 +0530

    banner removed background image

--- src/views/header/banner.jsx
+++ src/views/header/banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import '../../App.css';
 
 export const Banner = () => (
-    <div className="site-blocks-cover overlay" style={{ "background-image": "url(images/Free-Blue-Textured-Background-For-Download.jpg)" }} data-aos="fade" data-stellar-background-ratio="0.5" id="home-section">
+    <div className="site-blocks-cover overlay" data-aos="fade" data-stellar-background-ratio="0.5" id="home-section">
         <div className="container">
             <div className="row align-items-center text-center justify-content-center">
                 <div className="col-md-8">
