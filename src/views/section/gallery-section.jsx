c37a567900642cba776a426043e5521f2308fe1d
HRITHIK <hrithikrtiwari@gmail.com>
2020-08-07 18:36:14 +0530

    gallery images import

--- src/views/section/gallery-section.jsx
+++ src/views/section/gallery-section.jsx
@@ -1,45 +1,83 @@
 import React from 'react';
+import 'views/section/styles/gallery.css';
+import gallery1 from 'images/gallery/img1.jpg';
+import gallery2 from 'images/gallery/img2.jpg';
+import gallery3 from 'images/gallery/img3.jpg';
+import gallery4 from 'images/gallery/img4.jpg';
+import gallery5 from 'images/gallery/img5.jpg';
+import gallery6 from 'images/gallery/img6.jpg';
+import gallery7 from 'images/gallery/img7.jpg';
+import gallery8 from 'images/gallery/img8.jpg';
+import gallery9 from 'images/gallery/img9.jpg';
+import gallery10 from 'images/gallery/img10.jpg';
+import gallery11 from 'images/gallery/img11.jpg';
+import gallery12 from 'images/gallery/img12.jpg';
+import gallery13 from 'images/gallery/img13.jpg';
+import gallery14 from 'images/gallery/img14.jpg';
+import gallery15 from 'images/gallery/img15.jpg';
+import gallery16 from 'images/gallery/img16.jpg';
+import gallery17 from 'images/gallery/img17.jpg';
+import gallery18 from 'images/gallery/img18.jpg';
+import gallery19 from 'images/gallery/img19.jpg';
+import gallery20 from 'images/gallery/img20.jpg';
+import gallery21 from 'images/gallery/img21.jpg';
+import gallery22 from 'images/gallery/img22.jpg';
+import gallery23 from 'images/gallery/img23.jpg';
+import gallery24 from 'images/gallery/img24.jpg';
+import gallery25 from 'images/gallery/img25.jpg';
+import gallery26 from 'images/gallery/img26.jpg';
+import gallery27 from 'images/gallery/img27.jpg';
+import gallery28 from 'images/gallery/img28.jpg';
+import gallery29 from 'images/gallery/img29.jpg';
+import gallery30 from 'images/gallery/img30.jpg';
+import gallery31 from 'images/gallery/img31.jpg';
+import gallery32 from 'images/gallery/img32.jpg';
+import gallery33 from 'images/gallery/img33.jpg';
+import gallery34 from 'images/gallery/img34.jpg';
+import gallery35 from 'images/gallery/img35.jpg';
+import gallery36 from 'images/gallery/img36.jpg';
+import gallery37 from 'images/gallery/img37.jpg';
 
 export const GallerySection = () => (
     <section class="galleryingrid">
         <div class="container">
-            <div><img alt="gallery"  src="images/gallery/img1.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img2.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img3.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img4.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img5.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img6.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img7.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img8.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img9.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img10.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img11.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img12.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img13.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img14.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img15.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img16.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img17.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img18.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img19.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img20.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img21.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img22.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img23.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img24.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img25.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img26.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img27.jpg" /></div>
-            <div class="vertical"><img alt="gallery"  src="images/gallery/img28.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img29.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img30.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img31.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img32.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img33.jpg" /></div>
-            <div class="horizontal"><img alt="gallery"  src="images/gallery/img34.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img35.jpg" /></div>
-            <div class="big"><img alt="gallery"  src="images/gallery/img36.jpg" /></div>
-            <div><img alt="gallery"  src="images/gallery/img37.jpg" /></div>
+            <div><img alt="gallery1" src={gallery1} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery2} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery3} /></div>
+            <div><img alt="gallery" src={gallery4} /></div>
+            <div><img alt="gallery" src={gallery5} /></div>
+            <div class="big"><img alt="gallery" src={gallery6} /></div>
+            <div><img alt="gallery" src={gallery7} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery8} /></div>
+            <div><img alt="gallery" src={gallery9} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery10} /></div>
+            <div><img alt="gallery" src={gallery11} /></div>
+            <div class="big"><img alt="gallery" src={gallery12} /></div>
+            <div><img alt="gallery" src={gallery13} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery14} /></div>
+            <div><img alt="gallery" src={gallery15} /></div>
+            <div class="big"><img alt="gallery" src={gallery16} /></div>
+            <div><img alt="gallery" src={gallery17} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery18} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery19} /></div>
+            <div><img alt="gallery" src={gallery36} /></div>
+            <div class="big"><img alt="gallery" src={gallery20} /></div>
+            <div><img alt="gallery" src={gallery21} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery22} /></div>
+            <div><img alt="gallery" src={gallery23} /></div>
+            <div class="big"><img alt="gallery" src={gallery24} /></div>
+            <div><img alt="gallery" src={gallery25} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery26} /></div>
+            <div class="vertical"><img alt="gallery" src={gallery27} /></div>
+            <div><img alt="gallery" src={gallery28} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery37} /></div>
+            <div><img alt="gallery" src={gallery29} /></div>
+            <div class="big"><img alt="gallery" src={gallery30} /></div>
+            <div><img alt="gallery" src={gallery31} /></div>
+            <div class="horizontal"><img alt="gallery" src={gallery32} /></div>
+            <div><img alt="gallery" src={gallery33} /></div>
+            <div><img alt="gallery" src={gallery34} /></div>
+            <div><img alt="gallery" src={gallery35} /></div>
         </div>
         <a href="gallery/index.html">View More Images</a>
     </section>
