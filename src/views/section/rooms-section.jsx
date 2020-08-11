a9a8a1deec942289d376d781255fcf0dfe740962
HRITHIK <hrithikrtiwari@gmail.com>
2020-08-11 15:28:21 +0530

    Rooms Section

--- src/views/section/rooms-section.jsx
+++ src/views/section/rooms-section.jsx
@@ -6,6 +6,7 @@ import DormitoryRoom1 from 'images/rooms/dormitoryrooms.png';
 import European from 'images/rooms/european.jpg';
 import French from 'images/rooms/french.jpg';
 import TreeHouse from 'images/rooms/tree-house.jpg';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 export const RoomsSection = () => (
     <div className="site-section" id="rooms-section">
@@ -19,45 +20,45 @@ export const RoomsSection = () => (
             </div>
             <div className="row">
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_1.jpg" className="d-block figure" data-fancybox="gallery">
-                        <img src={TreeHouse} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home">Tree House</a></h3>
+                        <Link to="images/img_1.jpg" className="d-block figure" data-fancybox="gallery">
+                            <img src={TreeHouse} />
+                        </Link>
+                    <h3 className="mb-0"><Link to="#home">Tree House</Link></h3>
                     <span className="text-muted">Interior</span>
                 </div>
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_2.jpg" className="d-block figure" data-fancybox="gallery">
+                    <Link to="images/img_2.jpg" className="d-block figure" data-fancybox="gallery">
                         <img src={French} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home">French Rooms</a></h3>
+                    </Link>
+                    <h3 className="mb-0"><Link to="#home">French Rooms</Link></h3>
                     <span className="text-muted">Interior</span>
                 </div>
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_3.jpg" className="d-block figure" data-fancybox="gallery">
+                    <Link to="images/img_3.jpg" className="d-block figure" data-fancybox="gallery">
                         <img src={European} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home">European Rooms</a></h3>
+                    </Link>
+                    <h3 className="mb-0"><Link to="#home">European Rooms</Link></h3>
                     <span className="text-muted">Interior</span>
                 </div>
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_4.jpg" className="d-block figure" data-fancybox="gallery">
+                    <Link to="images/img_4.jpg" className="d-block figure" data-fancybox="gallery">
                         <img src={DormitoryRoom1} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home">Conference Hall</a></h3>
+                    </Link>
+                    <h3 className="mb-0"><Link to="#home">Conference Hall</Link></h3>
                     <span className="text-muted">Interior</span>
                 </div>
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_5.jpg" className="d-block figure" data-fancybox="gallery">
+                    <Link to="images/img_5.jpg" className="d-block figure" data-fancybox="gallery">
                         <img src={DormitoryRoom} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home"> Dormitory Room</a></h3>
+                    </Link>
+                    <h3 className="mb-0"><Link to="#home"> Dormitory Room</Link></h3>
                     <span className="text-muted">Design</span>
                 </div>
                 <div className="col-lg-4 col-md-6 mb-4 project-entry">
-                    <a href="images/img_6.jpg" className="d-block figure" data-fancybox="gallery">
+                    <Link to="images/img_6.jpg" className="d-block figure" data-fancybox="gallery">
                         <img src={DiningHall} />
-                    </a>
-                    <h3 className="mb-0"><a href="#home">Dining Hall</a></h3>
+                    </Link>
+                    <h3 className="mb-0"><Link to="#home">Dining Hall</Link></h3>
                     <span className="text-muted">Design</span>
                 </div>
             </div>
