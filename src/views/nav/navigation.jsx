920f9ad849eeff64e175d0ea1f5fe5ee66fc96da
HRITHIK <hrithikrtiwari@gmail.com>
2020-08-11 15:28:55 +0530

    Router Links

--- src/views/nav/navigation.jsx
+++ src/views/nav/navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logoImage from 'images/logo.jpg';
 import '../nav/styles/nav.sass';
+import {Link} from 'react-router-dom';
 
 
 export const Navigation = () => (
@@ -11,21 +12,21 @@ export const Navigation = () => (
                 <div className="container py-3">
                     <div className="row align-items-center">
                         <div className="col-6">
-                            <a href="#home" className="p-2 pl-0"><span className="icon-twitter" /></a>
-                            <a href="http://facebook.com/heavenhill.3" className="p-2 pl-0"><span className="icon-facebook" /></a>
-                            <a href="http://www.instagram.com/heaven_hill_resort" className="p-2 pl-0"><span className="icon-instagram" /></a>
-                            <a href="tel:9021184505" className="p-2 pl-0"><span className="icon-phone" /></a>
+                            <Link to="#home" className="p-2 pl-0"><span className="icon-twitter" /></Link>
+                            <Link to="http://facebook.com/heavenhill.3" className="p-2 pl-0"><span className="icon-facebook" /></Link>
+                            <Link to="http://www.instagram.com/heaven_hill_resort" className="p-2 pl-0"><span className="icon-instagram" /></Link>
+                            <Link to="tel:9021184505" className="p-2 pl-0"><span className="icon-phone" /></Link>
                         </div>
                         <div className="col-6">
                             <div className="d-flex ml-auto">
-                                <a href="#home" className="d-flex align-items-center ml-auto mr-4">
+                                <Link to="#home" className="d-flex align-items-center ml-auto mr-4">
                                     <span className="icon-envelope mr-2" />
                                     <span className="d-none d-md-inline-block">heavenhillagroresort@gmail.com</span>
-                                </a>
-                                <a href="#home" className="d-flex align-items-center">
+                                </Link>
+                                <Link to="#home" className="d-flex align-items-center">
                                     <span className="icon-phone mr-2" />
                                     <span className="d-none d-md-inline-block">+91 90211 84505</span>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -35,42 +36,42 @@ export const Navigation = () => (
                 <div className="container">
                     <div className="row align-items-center">
                         <div className="col-2">
-                            <h1 className="my-0 site-logo"><a href="index.html">
+                            <h1 className="my-0 site-logo"><Link to="index.html">
                                 <img src={logoImage} className="site-logo-image" alt="logo" />
                                 {/*the image logo  */}
-                            </a>
+                            </Link>
                             </h1>
                         </div>
                         <div className="col-10">
                             <nav className="site-navigation text-right" role="navigation">
                                 <div className="container">
-                                    <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#home" className="site-menu-toggle js-menu-toggle text-white"><span className="icon-menu h3" /></a></div>
+                                    <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><Link to="#home" className="site-menu-toggle js-menu-toggle text-white"><span className="icon-menu h3" /></Link></div>
                                     <ul className="site-menu main-menu js-clone-nav d-none d-lg-block">
                                         <li>
-                                            <a href="#home-section" className="nav-link">Home</a>
+                                            <Link to="#home-section" className="nav-link">Home</Link>
                                         </li>
                                         <li className="has-children">
-                                            <a href="#about-section" className="nav-link">About Us</a>
+                                            <Link to="#about-section" className="nav-link">About Us</Link>
                                             <ul className="dropdown arrow-top">
-                                                <li><a href="#about-section" className="nav-link">About</a></li>
-                                                <li><a href="#pricing-section" className="nav-link">Pricing</a></li>
-                                                <li><a href="#faq-section" className="nav-link">FAQ</a></li>
+                                                <li><Link to="#about-section" className="nav-link">About</Link></li>
+                                                <li><Link to="#pricing-section" className="nav-link">Pricing</Link></li>
+                                                <li><Link to="#faq-section" className="nav-link">FAQ</Link></li>
                                                 <li className="has-children">
-                                                    <a href="#home">Gallery</a>
+                                                    <Link to="gallery">Gallery</Link>
                                                     <ul className="dropdown">
-                                                        <li><a href="#home">Rooms</a></li>
-                                                        <li><a href="#home">Picnics</a></li>
-                                                        <li><a href="#home">Environment</a></li>
+                                                        <li><Link to="#home">Rooms</Link></li>
+                                                        <li><Link to="#home">Picnics</Link></li>
+                                                        <li><Link to="#home">Environment</Link></li>
                                                     </ul>
                                                 </li>
                                             </ul>
                                         </li>
                                         <li>
-                                            <a href="#rooms-section" className="nav-link">Rooms</a>
+                                            <Link to="#rooms-section" className="nav-link">Rooms</Link>
                                         </li>
-                                        <li><a href="#pricing-chart-section" className="nav-link">Pricing</a></li>
-                                        <li><a href="#features-section" className="nav-link">Features</a></li>
-                                        <li><a href="#contact-section" className="nav-link">Contact</a></li>
+                                        <li><Link to="#pricing-chart-section" className="nav-link">Pricing</Link></li>
+                                        <li><Link to="#features-section" className="nav-link">Features</Link></li>
+                                        <li><Link to="#contact-section" className="nav-link">Contact</Link></li>
                                     </ul>
                                 </div>
                             </nav>
