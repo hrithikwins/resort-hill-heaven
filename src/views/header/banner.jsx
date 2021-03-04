7400d1bab21cd68ed07d769d263c4abbf4531b72
hrithikrtiwari <hrithikrtiwari@gmail.com>
2021-03-04 21:26:45 +0530

    ..

--- src/views/header/banner.jsx
+++ src/views/header/banner.jsx
@@ -5,36 +5,58 @@ import "../header/styles/banner.sass";
 
 export const Banner = () => (
   <>
-    <div class="fullscreen-bg">
-      <img src={heroBg} alt="background" />
-    </div>
-    <div
-      className="site-blocks-cover overlay"
-      data-aos="fade"
-      data-stellar-background-ratio="0.5"
-      id="home-section"
-    >
-      <div className="banner-background-gif">
-        <div className="row align-items-center text-center justify-content-center">
-          <div className="col-md-8">
-            <a
-              data-fancybox
-              data-ratio={2}
-              href="#home"
-              className="play-button d-block"
-            >
-                <img src={logoImage} alt="logo" className="banner-img" />
-            </a>
-
-            <h1 className="text-uppercase bubblegum-font">
-              Heaven Hill Resort
-            </h1>
-            <span className="sub-text mb-3 d-block text-orange">
-              <em>#Refined By Nature</em>
-            </span>
+    <header class="masthead vh-100 d-flex justify-content-center align-items-center">
+      <div class="intro-body">
+        <div class="container">
+          <div class="row intro-container">
+            <div class="p-4 col-lg-8 mx-auto">
+              <h1 class="brand-heading p-lg-4 text-center">
+                <span className="jumping-text-container">
+                  <span className="n-1">H</span>
+                  <span className="n-2">E</span>
+                  <span className="n-3">A</span>
+                  <span className="n-4">V</span>
+                  <span className="n-5">E</span>
+                  <span className="n-6">N</span>
+                  <span className="n-7">&nbsp;</span>
+                  <span className="n-8">H</span>
+                  <span className="n-9">I</span>
+                  <span className="n-10">L</span>
+                  <span className="n-11">L</span>
+                  <span className="n-12">&nbsp;</span>
+                  <span className="n-13">R</span>
+                  <span className="n-14">E</span>
+                  <span className="n-15">S</span>
+                  <span className="n-16">O</span>
+                  <span className="n-17">R</span>
+                  <span className="n-18">T</span>
+                </span>
+              </h1>
+              <p
+                class="intro-text text-center"
+                data-aos="fade-up"
+                data-aos-duration="3700"
+                data-aos-delay="4000"
+              >
+                Somewhere among the hills, when the sun sets, new experiences
+                find their way. Come to experience the unleashed joy and
+                surprises amongst the nature.
+              </p>
+              <div class="d-flex justify-content-center">
+                <button
+                  class="glass-blue-btn"
+                  type="button"
+                  data-aos="fade-up"
+                  data-aos-duration="4500"
+                  data-aos-delay="7000"
+                >
+                  Book Now
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   </>
 );
